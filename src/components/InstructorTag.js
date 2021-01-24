dcc3ab159787c7b069bbb5edaf30d9beeb242dd0
aakhunaizi <62260811+aakhunaizi@users.noreply.github.com>
2021-01-24 10:31:19 +0300

    Added instructors.js

--- src/components/InstructorTag.js
+++ src/components/InstructorTag.js
@@ -1,7 +1,17 @@
+import instructors from "../instructors";
 const InstructorTag = () => {
   // Create the InstructorTag component here
-
-  return <></>;
+  const instructorTag = instructors.map((instructor) => {
+    return <InstructorTag key={instructor.id} instructor={instructor} />;
+  });
+  return (
+    <div className="AppWrapper">
+      <h2>When in doubt, ask for help!</h2>
+      <h1>{instructorTag.emoji}</h1>
+      <h1>{instructorTag.name}</h1>
+      <h1>{instructorTag.github}</h1>
+    </div>
+  );
 };
 
 export default InstructorTag;
---
+++ src/instructors.js
@@ -0,0 +1,22 @@
+const instructors = [
+  {
+    id: 1,
+    emoji: "🌚",
+    name: "Hamza",
+    github: "https://github.com/DarthHamza",
+  },
+  {
+    id: 2,
+    emoji: "🐥",
+    name: "Laila",
+    github: "https://github.com/Lailz",
+  },
+  {
+    id: 3,
+    emoji: "🦍",
+    name: "Hasan",
+    github: "https://github.com/thehasanas",
+  },
+];
+
+export default instructors;
